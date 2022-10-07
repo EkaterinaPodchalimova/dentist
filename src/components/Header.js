f272e83bcf0174d49c0117a2ec3d35fbff3e8e21
Ekaterina Podchalimova <pod4alimova.ekaterina@yandex.ru>
2022-10-07 12:04:38 +0300

    feat: mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ function Header({AboutPage,Prise, Certificate, Contacts}) {
 
     const handleButtonMenu = () => {
         editMenu(!menu)
-        console.log(menu)
     }
 
     return (
@@ -14,11 +13,11 @@ function Header({AboutPage,Prise, Certificate, Contacts}) {
             <p className="header__title">Ариадна</p>
             <button className='header__menu-button' onClick={handleButtonMenu}/>
             <div className={`header__menu  ${menu ? 'header__menu_active' : ''}`}>
-                <Link to='/' className={`header__navigation ${AboutPage && 'header__navigation_active'}`}>О Клинике</Link>
-                <Link to='/prise' className={`header__navigation ${Prise && 'header__navigation_active'}`}>Прайс</Link>
-                <Link to='/certificates' className={`header__navigation ${Certificate && 'header__navigation_active'}`}>Сертификаты</Link>
-                <Link to='/contacts' className={`header__navigation ${Contacts && 'header__navigation_active'}`}>Контакты</Link>
-                <a className="header__button" href='https://yandex.ru/business/widget/request/company/166797399456' target='_blank'>Заказать звонок</a>
+                <Link to='/' className={`header__navigation ${AboutPage && 'header__navigation_active'}`} onClick={() => {editMenu(false)}}>О Клинике</Link>
+                <Link to='/prise' className={`header__navigation ${Prise && 'header__navigation_active'}`} onClick={() => {editMenu(false)}}>Прайс</Link>
+                <Link to='/certificates' className={`header__navigation ${Certificate && 'header__navigation_active'}`} onClick={() => {editMenu(false)}}>Сертификаты</Link>
+                <Link to='/contacts' className={`header__navigation ${Contacts && 'header__navigation_active'}`} onClick={() => {editMenu(false)}}>Контакты</Link>
+                <a className="header__button" href='https://yandex.ru/business/widget/request/company/166797399456' target='_blank' onClick={() => {editMenu(false)}}>Заказать звонок</a>
             </div>
         </header>
     )
